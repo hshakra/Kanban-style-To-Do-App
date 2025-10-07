7d8fb239c3ac3986a98ccf8f2b007ac73f9d1c41
husam <hs407359@dal.ca>
2025-10-07 16:26:15 -0300

    First commit: functioning spring boot kanban app

---
+++ src/main/java/com/husam/kanban/KanbanStyleToDoAppApplication.java
@@ -0,0 +1,13 @@
+package com.husam.kanban;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KanbanStyleToDoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KanbanStyleToDoAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/husam/kanban/KanbanStyleToDoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.husam.kanban;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KanbanStyleToDoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
